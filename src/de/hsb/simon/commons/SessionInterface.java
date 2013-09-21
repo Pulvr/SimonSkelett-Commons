f6338c05964419be0beedfa425da979f6ba7f4b6
Florian <pulv3rtoastmann@gmail.com>
2013-09-21 14:57:21 +0200

    erneuter test

---
+++ src/de/hsb/simon/commons/SessionInterface.java
@@ -0,0 +1,9 @@
+package de.hsb.simon.commons;
+
+import java.io.Serializable;
+
+import de.root1.simon.SimonUnreferenced;
+
+public interface SessionInterface extends Serializable, SimonUnreferenced {
+	
+}
